9e71d6505b3c085c7a5240f0805a21a1b8a9b1c0
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-07-01 19:48:35 +0200

    removing acme and creating user entity

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Laboratory\Bundle\ManagementBundle\LaboratoryManagementBundle(),
             new Nakard\Laboratory\ApplicationBundle\NakardLaboratoryApplicationBundle(),
         );
 
