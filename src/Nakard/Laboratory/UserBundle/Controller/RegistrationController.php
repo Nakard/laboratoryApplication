29f6d2df51dc1fe5152d679e08d805962de81773
Arkadiusz Moskwa <a.moskwa@gmail.com>
2014-07-05 21:02:53 +0200

    overriding controllers

--- src/Nakard/Laboratory/UserBundle/Controller/RegistrationController.php
+++ src/Nakard/Laboratory/UserBundle/Controller/RegistrationController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * UserController.php
+ * RegistrationController.php
  *
  * Creation date: 2014-07-05
  * Creation time: 10:49
@@ -14,13 +14,47 @@ use Nakard\Laboratory\UserBundle\Entity\Users\User;
 use Nakard\Laboratory\UserBundle\Form\Users\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FOS\UserBundle\Controller\RegistrationController as BaseController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
- * Class UserController
+ * Class RegistrationController
  * @package Nakard\Laboratory\UserBundle\Controller
  */
-class UserController extends Controller
+class RegistrationController extends BaseController
 {
+    public function registerAction()
+    {
+        $form = $this->container->get('fos_user.registration.form');
+        $formHandler = $this->container->get('fos_user.registration.form.handler');
+        $confirmationEnabled = $this->container->getParameter('fos_user.registration.confirmation.enabled');
+
+        $process = $formHandler->process($confirmationEnabled);
+        if ($process) {
+            $user = $form->getData();
+
+            $this->container->get('logger')->info(
+                sprintf('New user registration: %s', $user)
+            );
+
+            if ($confirmationEnabled) {
+                $this->container->get('session')->set('fos_user_send_confirmation_email/email', $user->getEmail());
+                $route = 'fos_user_registration_check_email';
+            } else {
+                $this->authenticateUser($user);
+                $route = 'fos_user_registration_confirmed';
+            }
+
+            $this->setFlash('fos_user_success', 'registration.flash.user_created');
+            $url = $this->container->get('router')->generate($route);
+
+            return new RedirectResponse($url);
+        }
+
+        return $this->container->get('templating')->renderResponse('FOSUserBundle:Registration:register.html.'.$this->getEngine(), array(
+            'form' => $form->createView(),
+        ));
+    }
 
     public function indexAction()
     {
---
+++ src/Nakard/Laboratory/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * RegistrationFormType.php
+ *
+ * Creation date: 2014-07-05
+ * Creation time: 20:54
+ *
+ * @author Arkadiusz Moskwa <a.moskwa@gmail.com>
+ */
+
+namespace Nakard\Laboratory\UserBundle\Form\Type;
+
+
+class RegistrationFormType extends  
+{
+
+} 
